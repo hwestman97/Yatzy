ff41a717b7045b1f2f5a0156a7acce38cbef3406
Hanna <ha.westman@gmail.com>
2018-11-09 18:22:37 +0100

    .

--- dice.py
+++ dice.py
@@ -1,20 +1,6 @@
-"""
-YATZY!!!!!!!!!!
-"""
 
 from random import randint
 from time import sleep
-
-class Player():
-    
-    def __init__(self, name):
-        self.name = name
-        
-    def points(self):
-        pass
-    
-    def rolls_left(self):
-        pass
 
 def roll_dice():
     '''
@@ -26,30 +12,4 @@ def roll_dice():
     print ('The die roll is a %d' % dice_roll)
     return dice_roll
 
-def re_roll(n):
-    '''
-    Kallar på funktionen dice_roll n gånger
-    '''
-    pass
-
-def save_dice(lista):
-    '''
-    Lagrar tärninskasten i en lista, ska också fråga spelaren vilka 
-    tärningar som ska sparas och kalla på roll_dice för att kasta om
-    resterande tärningar
-    '''
-    pass
-
-def score(tärningskast):
-    '''
-    Håller koll på hur mycket poäng spelaren har
-    '''
-    pass
-
-def main():
-    '''
-    Kallar bara på andra funktioner
-    '''
-    pass
-
-main()
+roll_dice()
